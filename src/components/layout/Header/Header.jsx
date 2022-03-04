e29ff9af8a8df5e0eb79f349be9d9640462c52db
Monfear <wnuq@ymail.com>
2022-03-04 16:23:53 +0100

    improve header

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -4,6 +4,8 @@ import pokerCards from './../../../assets/video/poker-cards.mp4';
 import { Container } from './../Container/Container';
 import { Button } from '../../interface/Button/Button';
 
+import spade from './../../../assets/img/spade.png';
+
 export const Header = () => {
     return (
         <header className={styles.Header}>
@@ -14,13 +16,16 @@ export const Header = () => {
 
             <Container>
                 <div className={styles.Header__content}>
+                    <img src={spade} alt='spade' className={styles.Header__content__spade} />
                     <h1 className={styles.Header__content__heading}>join our elite royal spades poker club</h1>
-                    <h2 className={styles.Header__content__subheading}>free vip access</h2>
+                    <h2 className={styles.Header__content__subheading}>
+                        free <span>vip</span> access
+                    </h2>
                     <p className={styles.Header__content__caption}>Royal Spades Poker club provides all of our members with an upscale atmosphere that is both safe and comfortable.</p>
 
                     <Button
                         styleModifier={{
-                            marginTop: '4rem',
+                            marginTop: '6rem',
                         }}
                     >
                         find out more
